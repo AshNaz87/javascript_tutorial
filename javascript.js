e9b81d887a579a1e6f20734663071fa2b89a84fc
Ashraf Nazar <ashraf.caspian@gmail.com>
2017-07-09 01:11:17 +0100

    added new completed Le Wagon exercise

--- javascript.js
+++ javascript.js
@@ -113,6 +113,56 @@ function splitTheBill(group) {
 
 module.exports = splitTheBill;  // Do not remove. We need this for the spec to know about your method.
 
+function runChallenges() {
+
+  // Ex 1. Read the content of the email input
+  var email = $('#email').val();  // TODO: replace null, keep the email variable.
+
+  // Ex 2. Fill the content of the email input with your email
+  $('#email').val('ashraf.caspian@gmail.com');
+
+  // Ex 3. Replace the email hint (next to the input) with 'This is my email now'
+  //       The text should be emphasized using a <strong> tag
+  var emailHint = $('#email-hint');
+  var text = $('<strong>').text("This is my email now");
+    emailHint.html(text);
+
+
+  // Ex 4. Add the .blue CSS class to the table header cells (th elements)
+  var blue = $('th').addClass('blue');
+
+
+  // Ex 5. Count the number of table body rows there are (team count!)
+  // TODO: replace 0, keep the team_count variable.
+  var teamCount = $('tbody tr').length;
+
+  // Ex 6. Say there is a 15th team added to the table.
+  //       Add a row at the bottom, this new team should have zero points.
+  var row = $('<tr>').html('<td>' + (teamCount + 1) + '</td><td>New Team</td><td>0</td></tr>');
+  $('tbody').append(row);
+
+
+  // Ex 7. Write some code to sum all points given to all teams
+  // TODO: replace 0 with your sum result, keep the sum variable.
+  var sum = 0;
+  var scores = $('tbody tr td:last-child');
+  scores.each(function(i, item) {
+    sum += parseInt($(item).text())
+  });
+
+
+  // Ex 8. Change the background color of all table header cells to #DDF4FF
+  $('th').css({'background-color': '#DDF4FF'});
+
+
+  // Ex 9. Translate the #results list 20px downward (animation would be great ^_^)
+  $('#results').css({'transform': 'translateY(20px)'});
+
+  // Ex 10. Remove the "Email:" label from the DOM
+  $('label').remove();
+
+
+
 
 
 
